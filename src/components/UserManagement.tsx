542f4afa9de49e23401d19ebe5ff0f572153d53f
wichitpanw <wichitpanw@users.noreply.github.com>
2026-05-29 07:55:47 +0700

    fix: disable storage on temporary supabase client to avoid warning

--- src/components/UserManagement.tsx
+++ src/components/UserManagement.tsx
@@ -102,7 +102,12 @@ export default function UserManagement() {
         auth: {
           persistSession: false,
           autoRefreshToken: false,
-          detectSessionInUrl: false
+          detectSessionInUrl: false,
+          storage: {
+            getItem: () => null,
+            setItem: () => {},
+            removeItem: () => {},
+          }
         }
       });
 
